8b8ebca051ca4de087d0b55fa85d1cb1a1efebc5
Andreas Reich (@LAPTOP) <r_andreas2@web.de>
2014-11-08 17:40:12 +0100

    Basic GameManager setup with turns, score and time.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    private class Turn
+    {
+        private GameObject _removedObject0;
+        private GameObject _removedObject1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        Turn(GameObject removedObject0, GameObject removedObject1)
+        {
+            _removedObject0 = removedObject0;
+            _removedObject1 = removedObject1;
+        }
+
+        public void Do()
+        {
+            // TODO: Call select
+            _removedObject0.SetActive(false);
+            _removedObject0.SetActive(false);
+        }
+        public void Undo()
+        {
+            // TODO: Call unselect
+            _removedObject0.SetActive(true);
+            _removedObject0.SetActive(true);
+        }
+    };
+
+    private GameObject[, ,] _level;
+    private GameObject _selectedObject = null;
+    private Stack<Turn> _turns = new Stack<Turn>();
+
+    public uint Score { get; private set; }
+    public float RoundTime { get; private set; }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartRound(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RoundTime += Time.deltaTime;
+    }
+
+    void StartRound(uint roundIndex)
+    {
+        // Reset round properties
+        _selectedObject = null;
+        _turns.Clear();
+        Score = 0;
+        RoundTime = 0.0f;
+
+        // TODO: Generate new level
+    }
 }
