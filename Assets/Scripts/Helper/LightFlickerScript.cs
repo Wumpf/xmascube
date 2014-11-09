b60df7fb8f0a93009d4590e78bbc9c501a9d9900
gebert <gebertenrico@googlemail.com>
2014-11-09 12:44:58 +0100

    some effects

---
+++ Assets/Scripts/Helper/LightFlickerScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlickerScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        light.intensity = Mathf.Max(4.2f, Mathf.Min(5.75f, light.intensity + Random.Range(-0.2f, 0.2f)));
+	}
+}
